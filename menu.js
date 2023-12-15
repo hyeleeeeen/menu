79550eae1fc0d14f224014ec9a985357cb6f749a
hyeleeeeen <heo22969087@gmail.com>
2023-12-15 13:17:21 +0900

    새로운 메뉴판 생성

---
+++ menu.js
@@ -0,0 +1,42 @@
+const $title = document.querySelector("h1");
+const $meals = document.getElementById("meals");
+const $sides = document.getElementById("sides");
+const $drinks = document.getElementById("drinks");
+const $bell = document.getElementById("bell");
+const $mainList = document.getElementById("mainList");
+
+const meals = ["뚜촌샤브샤브", "뚜볶이", "뚤리오올리오", "뚜치볶음밥"];
+const sides = ["만뚜", "감자뚜김", "뚜포", "뚜과자"];
+const drinks = ["진로", "화요", "테라", "콜라"];
+const bell = ["물", "티슈", "물티슈", "집갈래요"];
+
+const createList = (t) => { // 배열의 각 요소를 forEach를 통해 뿌려주기
+  t.forEach((v) => {
+    const menuItem = document.createElement("li"); // li 태그 생성
+    menuItem.textContent = v; // 생성한 태그에 배열의 요소를 입력하기
+    $mainList.append(menuItem); // 부모태그에 붙여넣기
+  });
+};
+
+const menuChoiceBtn = (e) => {
+  // 이전에 생성된 목록이 있다면 초기화
+  while ($mainList.firstChild) { // 자식요소 제거하기
+    $mainList.removeChild($mainList.firstChild);
+  }
+
+  if (e.target.textContent === "사이드") {
+    createList(sides);
+  } else if (e.target.textContent === "주류&음료") {
+    createList(drinks);
+  } else if (e.target.textContent === "식사") {
+    createList(meals);
+  } else {
+    createList(bell);
+  }
+};
+
+$title.addEventListener("click", () => location.reload());
+$meals.addEventListener("click", menuChoiceBtn);
+$sides.addEventListener("click", menuChoiceBtn);
+$drinks.addEventListener("click", menuChoiceBtn);
+$bell.addEventListener("click", menuChoiceBtn);
